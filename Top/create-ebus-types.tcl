3afaa2944ac691fe720f03c2baa82c12b7028057
Eric Hazen <hazen@bu.edu>
2022-03-08 10:15:26 -0500

    will it build?

---
+++ Top/create-ebus-types.tcl
@@ -0,0 +1,17 @@
+set PATH_REPO "[file normalize [file dirname [info script]]]/../"
+
+#https://www.xilinx.com/support/answers/72570.html
+set PYTHONPATH $::env(PYTHONPATH)
+set PYTHONHOME $::env(PYTHONHOME)
+set PATH $::env(PATH)
+unset env(PYTHONPATH)
+unset env(PYTHONHOME)
+unset env(PATH)
+set env(PATH) "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+set make_cmd "cd $PATH_REPO/picoblaze/sim && make"
+puts [exec bash -c $make_cmd]
+
+set env(PYTHONPATH) $PYTHONPATH
+set env(PYTHONHOME) $PYTHONHOME
+set env(PATH) $PATH
