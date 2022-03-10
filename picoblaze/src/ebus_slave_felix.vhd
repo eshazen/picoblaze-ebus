1814dc78d7c4e65e26777f02e2aa70ded7b7a961
Eric Hazen <hazen@bu.edu>
2022-03-10 15:33:39 -0500

    fixing up

--- picoblaze/src/ebus_slave_felix.vhd
+++ picoblaze/src/ebus_slave_felix.vhd
@@ -36,51 +36,51 @@ end entity ebus_slave_felix;
 
 architecture arch of ebus_slave_felix is
 
-  type ram_type is array ( 0 to RAM_DEPTH-1) of std_logic_vector(RAM_WIDTH-1 downto 0);
+  type ram_type is array (0 to RAM_DEPTH-1) of std_logic_vector(RAM_WIDTH-1 downto 0);
   signal RAM : ram_type;
 
   -- addresss offset for wide words
-  constant RAM_MUX_ADDR_BIT : integer := clog2( RAM_WIDTH/32);
+  constant RAM_MUX_ADDR_BIT : integer := clog2(RAM_WIDTH/32);
   -- number of words per RAM location
-  constant RAM_MUX_FACTOR : integer := integer( 2 ** RAM_MUX_ADDR_BIT);
+  constant RAM_MUX_FACTOR   : integer := integer(2 ** RAM_MUX_ADDR_BIT);
   -- width of RAM rounded up to 32 bits
-  constant RAM_MUX_WIDTH : integer := 32 * RAM_MUX_FACTOR;
+  constant RAM_MUX_WIDTH    : integer := 32 * RAM_MUX_FACTOR;
 
   -- address width
-  constant ADDR_WIDTH : integer := clog2( RAM_DEPTH);
+  constant ADDR_WIDTH : integer := clog2(RAM_DEPTH);
 
-  signal write_addr : unsigned( ADDR_WIDTH-1 downto 0);
-  signal read_addr : unsigned( ADDR_WIDTH-1 downto 0);
+  signal write_addr : unsigned(ADDR_WIDTH-1 downto 0);
+  signal read_addr  : unsigned(ADDR_WIDTH-1 downto 0);
 
-  signal mux_in : std_logic_vector( RAM_MUX_WIDTH-1 downto 0);
+  signal mux_in  : std_logic_vector(RAM_MUX_WIDTH-1 downto 0);
   signal mux_out : std_logic_vector(31 downto 0);
 
 begin  -- architecture arch
 
   -- asynchronously multiplex output
-  fg: for i in 31 downto 0 generate
-    mux_out(i) <= mux_in( 32 * to_integer(unsigned(ebus_out.addr(RAM_MUX_ADDR_BIT-1 downto 0))));
+  fg : for i in 31 downto 0 generate
+    mux_out(i) <= mux_in(32 * to_integer(unsigned(ebus_out.addr(RAM_MUX_ADDR_BIT-1 downto 0))));
   end generate fg;
 
-  mux_in( RAM_WIDTH-1 downto 0) <= ram_in;
+  mux_in(RAM_WIDTH-1 downto 0) <= ram_in;
 
   process (clk, reset) is
   begin  -- process
     if reset = '1' then                 -- asynchronous reset (active high)
 
       write_addr <= (others => '0');
-      read_addr <= (others => '0');
+      read_addr  <= (others => '0');
 
     elsif rising_edge(clk) then         -- rising clock edge
 
       -- write incoming data with wrap around
       if ram_wr = '1' then
-        RAM( to_integer( write_addr)) <= ram_in;
+        RAM(to_integer(write_addr)) <= ram_in;
         if write_addr /= RAM_DEPTH-1 then
           write_addr <= write_addr + 1;
         end if;
       end if;
-      
+
 
       -- decode address according to BASE_ADDR
       if std_match(std_logic_vector(ebus_out.addr), EBUS_BASE_ADDR) then
@@ -92,20 +92,23 @@ begin  -- architecture arch
           else
             if ebus_out.addr(0) = '0' then
               ebus_in.data(ADDR_WIDTH-1 downto 0) <= std_logic_vector(write_addr);
-              ebus_in.data(31 downto ADDR_WIDTH) <= (others => '0');
+              ebus_in.data(31 downto ADDR_WIDTH)  <= (others => '0');
             else
               ebus_in.data(ADDR_WIDTH-1 downto 0) <= std_logic_vector(read_addr);
-              ebus_in.data(31 downto ADDR_WIDTH) <= (others => '0');
+              ebus_in.data(31 downto ADDR_WIDTH)  <= (others => '0');
+            end if;
           end if;
+
         end if;
 
         if ebus_out.wr = '1' then
-          read_addr <= ebus_in.data(ADDR_WIDTH-1 downto 0);
+          read_addr <= unsigned(ebus_in.data(ADDR_WIDTH-1 downto 0));
         end if;
 
       end if;
 
     end if;
+
   end process;
 
 end architecture arch;
